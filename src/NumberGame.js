3a5aa9cf47b9df43c2463040c37c11c0eda99801
AnushaV1 <anush_chander@yahoo.com>
2020-09-17 16:23:27 -0700

    added restart button and count

--- src/NumberGame.js
+++ src/NumberGame.js
@@ -2,16 +2,28 @@ import React, {useState} from "react";
 import "./NumberGame.css";
 const NumberGame = (props) => {
     const genRandom = () => Math.floor(Math.random() * 10) + 1; 
+    const restart = () =>{
+        setTarget(genRandom())
+        setGuess(0);
+        setGuessCount(0);
+    }
+    const makeGuess = () => {
+        setGuess(genRandom());
+        setGuessCount(guessCount +1);
+    }
     const [guess, setGuess] = useState(genRandom());
     const [target,setTarget] = useState(genRandom());
     const isWinner = target === guess;
+    const [guessCount, setGuessCount] = useState(0);
     return (
     <div>
         <h1>Target Num: {target}</h1>
         <h1 className={isWinner? 'winner':'loser'}>Your Guess:{guess} </h1>
+        <h4>Guess # : {guessCount}</h4>
         {!isWinner &&  
-        <button onClick={()=> setGuess(genRandom())}>Generate Num</button>
+        <button onClick={makeGuess}>Generate Num</button>
         }
+        <button onClick={restart}>New Game</button>
         </div>
     )
 }
